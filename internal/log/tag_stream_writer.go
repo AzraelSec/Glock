6d6729ec12a03cf52dcfdb7f237b675780a2ee26
AzraelSec <me@azraelsec.sh>
2023-09-11 23:14:05 +0200

    fixup! feat: introduce a tag stream writer

--- internal/log/tag_stream_writer.go
+++ internal/log/tag_stream_writer.go
@@ -2,21 +2,46 @@ package log
 
 import (
 	"bytes"
-	"fmt"
+	"crypto/md5"
+	"encoding/binary"
 	"io"
+	"strings"
+
+	"github.com/fatih/color"
 )
 
 type TagStreamWriter struct {
 	tag            string
 	output         io.Writer
+	color          *color.Color
 	buffer         bytes.Buffer
-	newLineTouched bool
+	nl bool
+}
+
+var fgColors = []color.Attribute{
+	color.FgRed,
+	color.FgGreen,
+	color.FgYellow,
+	color.FgBlue,
+	color.FgMagenta,
+	color.FgCyan,
+}
+
+func colorByTag(tag string) *color.Color{
+	hash := md5.Sum([]byte(strings.ToLower(tag)))
+	index := int(binary.BigEndian.Uint32(hash[:])) % len(fgColors)
+
+	if index < 0 {
+		index = -index
+	}
+	return color.New(fgColors[index])
 }
 
 func NewTagStreamWriter(tag string, out io.Writer) *TagStreamWriter {
 	return &TagStreamWriter{
 		tag:    tag,
 		output: out,
+		color:  colorByTag(tag),
 		buffer: bytes.Buffer{},
 	}
 }
@@ -25,15 +50,15 @@ func (w *TagStreamWriter) Write(p []byte) (int, error) {
 	w.buffer.Reset()
 
 	for _, b := range p {
-		if !w.newLineTouched {
-			w.buffer.WriteString(fmt.Sprintf("[%s] ", w.tag))
-			w.newLineTouched = true
+		if !w.nl {
+			w.buffer.WriteString(w.color.Sprintf("[%s] ", w.tag))
+			w.nl = true
 		}
 
 		w.buffer.WriteByte(b)
 
 		if b == '\n' {
-			w.newLineTouched = false
+			w.nl = false
 		}
 	}
 
