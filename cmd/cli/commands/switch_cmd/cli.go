bb98f71a45aedc3dbd7ecdd0a8b1c641fa878cbf
AzraelSec <me@azraelsec.sh>
2023-10-22 21:23:41 +0200

    chore: remove unused cli results printing format

--- cmd/cli/commands/switch_cmd/cli.go
+++ cmd/cli/commands/switch_cmd/cli.go
@@ -1,55 +1,11 @@
 package switchcmd
 
-import (
-	"errors"
-	"fmt"
-
-	"github.com/AzraelSec/glock/internal/runner"
-	"github.com/AzraelSec/glock/pkg/git"
-	"github.com/fatih/color"
-)
-
 type cli struct {
 	*switchGit
 }
 
 func newCli(sg *switchGit) *cli {
 	return &cli{switchGit: sg}
-}
-func (s *cli) printPlaneResults(results []runner.Result[struct{}]) {
-	if len(results) != 0 {
-		color.Green("Switches:")
-	}
-
-	errs := []struct {
-		idx int
-		err error
-	}{}
-	for i, rs := range results {
-		if rs.Error != nil {
-			if errors.Is(rs.Error, git.InvalidReferenceErr) {
-				continue
-			}
-
-			errs = append(errs, struct {
-				idx int
-				err error
-			}{
-				idx: i,
-				err: rs.Error,
-			})
-			continue
-		}
-
-		fmt.Printf("\t -> %s\n", s.repos[i].Name)
-	}
-
-	if len(errs) != 0 {
-		color.Red("Errors:")
-	}
-	for _, err := range errs {
-		fmt.Printf("\t -> %s {%s}\n", s.repos[err.idx].Name, err.err.Error())
-	}
 }
 
 func (s *cli) run(target string, force bool) {
